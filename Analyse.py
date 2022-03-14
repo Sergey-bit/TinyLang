689668342667233ca410e244a0a386c99bb96586
Sergey-bit <71939250+Sergey-bit@users.noreply.github.com>
2022-03-14 15:55:49 +0500

    Update Analyse.py

--- Analyse.py
+++ Analyse.py
@@ -1,5 +1,32 @@
 from Parser import Parser
 
 
+class ItemOfTable:
+    INTEGER, STRING, FUNC, FLOAT = range(4)
+    
+    def __init__(self, name, type_):
+        self.name = name
+        self.type = type_
+    
+    def __cmp__(self, other: str):
+        return self.name == other
+
+
+class Table:
+    def __init__(self):
+        self.table = []
+    
+    def add(self, item: ItemOfTable):
+        self.table.append(item)
+    
+    def search(self, name: str):
+        for item in self.table:
+            if item == name:
+                return item.type
+        return 
+
+
 class Analyse:
-    parser = Parser()
+    def __init__(self):
+        self.table = Table()
+        self.program = Parser().parse([])
